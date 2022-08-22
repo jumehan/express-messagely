5268842cc7ca220d3f5fedf0afc9c088e853b315
Julie Han <juliemenghan@gmail.com>
2022-08-22 15:37:40 -0400

    user register and authenticate done

--- models/user.js
+++ models/user.js
@@ -1,5 +1,13 @@
 "use strict";
 
+/** User class for message.ly */
+
+const { NotFoundError } = require("../expressError");
+const db = require("../db");
+const bcrypt = require("bcrypt");
+
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
 /** User of the site. */
 
 class User {
@@ -9,16 +17,47 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (username,
+                          password,
+                          first_name,
+                          last_name,
+                          phone,
+                          join_at)
+          VALUES ($1, $2, $3, $4, $5, current_timestamp)
+          RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]);
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+          FROM users
+          WHERE username = $1`,
+      [username]);
+
+    const user = result.rows[0];
+
+    if (user) {
+      return (await bcrypt.compare(password, user.password) === true);
+    }
+
+    return false;
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+          SET last_login_at = current_timestamp
+          WHERE username = $1`,
+      [username]);
   }
 
   /** All: basic info on all users:
